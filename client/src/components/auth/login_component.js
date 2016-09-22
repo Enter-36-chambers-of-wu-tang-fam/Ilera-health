37a153d4367ecbc2dff22d7a0e3d9d0c5af1cfd6
Yara TP <ytercero36@gmail.com>
2016-09-21 21:21:45 -0400

    added login component

---
+++ client/src/components/auth/login_component.js
@@ -0,0 +1,17 @@
+// React
+import React, { Component } from 'react';
+
+const login = () => {
+    return (
+        <form>
+            <label>Username</label>
+            <input type="email" placeholder="Type username here..." />
+            <label>Password</label>
+            <input type="password" placeholder="Type password here..." />
+            <label>Re-Type Password</label>
+            <input type="password" placeholder="Please re-type password..." />
+        </form>
+    );
+}
+
+export default login; 
