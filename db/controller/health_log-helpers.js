0ca7a4924ea71a524c277be8404cd5b4c59519bb
Neekon <neekon91@gmail.com>
2016-09-22 21:28:46 -0700

    server routes link up closes #40

--- db/controller/health_log-helpers.js
+++ db/controller/health_log-helpers.js
@@ -5,20 +5,22 @@ const db = require('../dbConnect/connection.js');
 
 module.exports.health_log = {
 
+  get_health_logs: (params, cb) => {
+    // Get Request → /api/healthlog/:physid/:patid  [ limit 5]
+    let data = [params.id_patient];
+    const queryString = 'SELECT * FROM health_log WHERE id_patient=? LIMIT 5';
+    db.query(queryString, data, (error, results) => cb(error, results) );
+  },
+
   add_health_log: (params, cb) => {
     //  post to health_log
     let data = [params.date, params.note, params.photo_path, params.id_physician, params.id_patient];
     const queryString = 'INSERT INTO health_log(date, note, photo_path, id_physician, id_patient) \
       value (?, ?, ?, ?, ?)';
     db.query(queryString, data, (error, results) => cb(error, results) );
-  },
-
-  get_health_logs: (params, cb) => {
-    // Get Request → /api/healthlog/:physid/:patid  [ limit 5]
-    let data = [params.id_patient];
-    const queryString = 'SELECT * FROM health_log WHERE id_patient=? LIMIT 5';
-    db.query(queryString, data, (error, results) => cb(error, results) );
   }
+
+
   //  (params, cb) => {
   //   let data = [params.id_patient];
   //
