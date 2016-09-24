9481a5e9a6df094c0985fd1224c58642477af1a1
Yara TP <ytercero36@gmail.com>
2016-09-24 00:46:44 -0400

    form flow nearly there

--- client/src/components/auth/signup-component.js
+++ client/src/components/auth/signup-component.js
@@ -43,13 +43,18 @@ class SignupForm extends Component {
             <div>
                 <h2>Sign Up</h2>
                 <form onSubmit={ handleSubmit(props => this.onSubmit(props)) }>
+                    <Field name="userType" component="select">
+                        <option></option>
+                        <option value="Patient">Patient</option>
+                        <option value="Provider">Provider</option>
+                    </Field>
                     <Field name="first" type="text" component={this.renderField} label="First"/>
                     <Field name="last" type="text" component={this.renderField} label="Last"/>
                     <Field name="username" type="text" component={this.renderField} label="Username"/>
                     <Field name="password" type="password" component={this.renderField} label="Password"/>
                     <Field name="reTypePassword" type="password" component={this.renderField} label="Re-Type Password"/>
                     {error && <strong>{error}</strong>}
-                    <button type='submit'  className='btn'>Next</button>
+                    <button type='submit' className='btn'>Next</button>
                 </form>
             </div>
         );
--- client/src/routes.js
+++ client/src/routes.js
@@ -12,14 +12,22 @@ import PatientApp from './components/patient-app/patient-app.jsx';
 import ProviderApp from './components/physician-app/physician-app.jsx';
 import EmergencyContactForm from './containers/forms/emergency-contact-form.js';
 import InsuranceForm from './containers/forms/insurance-policy-form.js';
+import BackgroundForm from './containers/forms/demographic-form.js';
+import FormParent from './components/form-parent.js';
 
 export default (
     <Router path='/' component= { App } >
-        <Route path='provider' component={ InsuranceForm } >
-            <Route path='signin' component={ Signin} />
+        <IndexRoute component={ Signin }/>
+        <Route path='signin' component={ Signin } />
+        <Route path='provider' component={ ProviderApp } >
         </Route>
         <Route path='patient' component={ PatientApp } >
-            <Route path='signin' component={ Signin} />
+            <Route path="form" > 
+                <IndexRoute component={ BackgroundForm }/>
+                <Route path='background' component={ BackgroundForm } />
+                <Route path='emergencyContact' component={ EmergencyContactForm } />
+                <Route path='insurance' component={ InsuranceForm } />
+            </Route>
         </Route>
     </Router>
 );
