264c0365d6a2229141e20721604d3a68dfb081be
Michael Olorunnisola <sweetpianist1@gmail.com>
2016-09-28 23:54:41 -0400

    reducer fixed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,6 +5,7 @@ import { reducer as formReducer } from 'redux-form';
 import authenticated from './authentication-reducer.js';
 import messages from './messages-reducer.js';
 import contacts from './contacts-reducer.js';
+import appointments from './appointment-reducer.js';
 
 const rootReducer = combineReducers({
     authentication: authenticated,
