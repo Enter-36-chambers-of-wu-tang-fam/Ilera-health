5e32213501b1eb88c2e5257430adabfa6c0f5e17
Michael Olorunnisola <sweetpianist1@gmail.com>
2016-09-28 20:53:44 -0400

    auth appoitments closes #152

---
+++ client/src/actions/appointment.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export const getCurrentBookings = (physid) => {
+  let currAppts = {};
+  axios.get(`/api/patient/getappointment/${physid}`).then( dates => { //get all of the physicians currently booked appointments
+    dates.data.forEach(item => {
+      let date = item.date.slice(0,10);  
+      if(currAppts[date]) currAppts[date][item.time] = true;
+      else {
+        currAppts[date] = {};
+        currAppts[date][item.time] = true;
+      } 
+    })
+  })
+  .catch(err => console.log(err) );
+
+  return {
+    type: 'ALL_PHYSICIAN_APPOINTMENTS',
+    payload: currAppts
+  }
+}
+
+
+
+export const setAppointment = (data) => {
+  axios.post('/api/patient/setappointment', data).then(set => {
+   console.log("SUCCESSFUL POST?", set); 
+  })
+  .catch(err => console.log("POST ERROR", err));
+
+ return function(dispatch){
+  dispatch({
+    type: types.SET_APPOINTMENT,
+    payload: data
+  })
+ }
+}
+
