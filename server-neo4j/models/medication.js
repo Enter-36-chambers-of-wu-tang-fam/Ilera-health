2b2d84e78b81756b11f2508746017b420d940710
Neekon <neekon91@gmail.com>
2016-10-01 13:11:02 -0700

    neo4j-professional-signin closes #188

---
+++ server-neo4j/models/medication.js
@@ -0,0 +1,8 @@
+const gdb = require('../graphDb/graphConnect.js').graphdb;
+
+// CREATE TABLE `medication` (
+//   `id` INTEGER NOT NULL AUTO_INCREMENT,
+//   `drug_name` VARCHAR(100) NULL DEFAULT NULL,
+//   `details` VARCHAR(1000) NULL DEFAULT NULL,
+//   PRIMARY KEY (`id`)
+// );
