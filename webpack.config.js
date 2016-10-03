7f9538b6f15f05ee601e8b653fc3e85426d842fa
Neekon <neekon91@gmail.com>
2016-10-03 16:42:00 -0700

    restructure-frontend-folders-webpack closes #227

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 
-var APP_DIR = path.resolve(__dirname, 'client/src');
+var APP_DIR = path.resolve(`${__dirname}/client/src`);
 var BUILD_DIR = path.resolve(__dirname, 'client/dist');
 
 var config = {
-  entry: APP_DIR + '/index.js',
+  entry: `${APP_DIR}/main/index.js`,
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
@@ -14,6 +14,7 @@ var config = {
     extensions: ['', '.js', '.jsx']
   },
   module: {
+    devtool: 'inline-source-map',
     loaders: [
       {
         test: /\.jsx?$/,
@@ -25,7 +26,7 @@ var config = {
       }
     ]
   }
-  
+
 };
 
 module.exports = config;
