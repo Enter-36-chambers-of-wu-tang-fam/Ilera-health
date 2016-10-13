881977c786fddd96901e04a3e78a8c482206337e
Michael Olorunnisola <sweetpianist1@gmail.com>
2016-10-13 01:35:08 -0400

    authenticate fix

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -14,11 +14,10 @@ module.exports = (req, res, next) => {
   if (token) {
     jwt.verify(token, config.jwtSecret, (err, decoded) => {
       if (err) {
-        res.status(401).json({ error: 'Failed to authenticate' });
+        res.json({ error: 'Failed to authenticate' });
       } else {
         Patient.getPatientInfoByID({ uid: decoded.id }, (err,data) =>{
-          if (data.length === 0) res.status(404).json({ error: 'No such user'});
-          console.log("DECODED!!!",decoded.id)
+          if (err || !data) res.status(404).json({ error: 'No such user'});
           req.body.uid ? req.body.uid = decoded.id : undefined;
           req.params.uid ? req.params.uid = decoded.id : undefined;
           req.params.userid ? req.params.userid = decoded.id : undefined;
